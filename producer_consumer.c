92af53956b8ec69184474b6059df195a70885b91
niranjana687 <niranjana687@gmail.com>
2021-01-21 11:35:33 +0530

    producer consumer completed

---
+++ producer_consumer.c
@@ -0,0 +1,82 @@
+#include<semaphore.h>
+#include<pthread.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+/*  two counting semaphores - full and empty 
+    buffer is the critical section
+    full keeps track of number of items in the buffer 
+     empty keeps track of number of unnoccupied slots 
+*/
+
+#define MaxItems 5
+#define BufferSize 5
+
+sem_t empty;
+sem_t full;
+int in = 0;
+int out = 0;
+int buffer[BufferSize];
+pthread_mutex_t mutex;
+
+void *producer(void *pno) {
+
+    int item;
+    for(int i=0; i<MaxItems; i++) {
+
+        item = rand();
+        sem_wait(&empty);
+        pthread_mutex_lock(&mutex);
+        buffer[in] = item;
+        printf("Producer %d: insert item %d at %d\n", *((int*)pno), buffer[in], in);
+        in = (in+1)%BufferSize;
+        pthread_mutex_unlock(&mutex);
+        sem_post(&full);
+
+    }
+}
+
+void *consumer(void *cno) {
+
+    for(int i=0; i<MaxItems; i++) {
+        sem_wait(&full);
+        pthread_mutex_lock(&mutex);
+        int item = buffer[out];
+        printf("Consumer %d: remove item %d at %d\n", *((int*)cno), item, out);
+        out = (out+1)%BufferSize;
+        pthread_mutex_unlock(&mutex);
+        sem_post(&empty);
+    }
+}
+
+int main() {
+
+    pthread_t pro[5], con[5];
+    pthread_mutex_init(&mutex, NULL);
+    sem_init(&empty, 0, BufferSize);
+    sem_init(&full, 0, 0);
+
+    int a[5] = {1,2,3,4,5};
+
+    for(int i=0; i<5; i++) {
+        pthread_create(&pro[i], NULL, (void *)producer, (void*)&a[i]);
+    }
+
+    for(int i=0; i<5; i++) {
+        pthread_create(&con[i], NULL, (void*)consumer, (void*)&a[i]);
+    }
+
+    for(int i=0; i<5; i++) {
+        pthread_join(pro[i], NULL);
+    }
+
+    for(int i=0; i<5; i++) {
+        pthread_join(con[i], NULL);
+    }
+
+    pthread_mutex_destroy(&mutex);
+    sem_destroy(&full);
+    sem_destroy(&empty);
+}
+
+
